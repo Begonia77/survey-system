30652c71990f772b703e1fca89970689f69a8280
Starry516 <qiu.huiyang@foxmail.com>
2023-03-14 18:08:11 +0800

    查询操作

--- sql/operation.sql
+++ sql/operation.sql
@@ -5,18 +5,18 @@ USE surveydb;
 SELECT COUNT(*) FROM `survey_info` WHERE `created_user_id` = '';
 
 -- 查询用户创建的所有问卷
-SELECT * FROM `survey_info` WHERE  `created_user_id` = '1';
+SELECT * FROM `survey_info` WHERE  `created_user_id` = '';
 
 -- 查询问卷的所有问题
-SELECT * FROM `question_info` WHERE  `survey_id` = '1';
+SELECT * FROM `question_info` WHERE  `survey_id` = '';
 
 -- 查询问题的所有选项
-SELECT * FROM `option_info` WHERE `question_id` = '1';
+SELECT * FROM `option_info` WHERE `question_id` = '';
 
 -- 查询问卷的所有问题+选项
 SELECT qu.question_id, qu.question, op.content
     FROM `question_info` AS qu, `option_info` AS op
-    WHERE qu.`survey_id` = '1'
+    WHERE qu.`survey_id` = ''
       AND qu.`question_id` = op.`question_id`;
 
 -- 查询某个问卷的总填写次数
@@ -31,7 +31,7 @@ SELECT COUNT(*) FROM `fill_in` WHERE `option_id` = '';
 -- 查询某个问卷的填写情况
 SELECT qu.question_id, qu.question, op.content, COUNT(fi.`option_id`) SUM
     FROM `question_info` AS qu, `option_info` AS op, `fill_in` AS fi
-    WHERE qu.`survey_id` = '1'
+    WHERE qu.`survey_id` = ''
         AND qu.`question_id` = op.`question_id`
         AND op.`option_id` = fi.`option_id`
     GROUP BY fi.`option_id`;
