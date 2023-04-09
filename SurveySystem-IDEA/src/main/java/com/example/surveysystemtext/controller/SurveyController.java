6a663dc12ea02ab8b30fa82ae5741eed7664d88d
wryte123 <oepkf@foxmail.com>
2023-04-09 16:58:35 +0800

    2023-04-09

--- SurveySystem-IDEA/src/main/java/com/example/surveysystemtext/controller/SurveyController.java
+++ SurveySystem-IDEA/src/main/java/com/example/surveysystemtext/controller/SurveyController.java
@@ -1,6 +1,5 @@
 package com.example.surveysystemtext.controller;
 
-import com.example.surveysystemtext.controller.vo.GPTDescripeVO;
 import com.example.surveysystemtext.entity.*;
 import com.example.surveysystemtext.mapper.OptionMapper;
 import com.example.surveysystemtext.mapper.QuestionMapper;
@@ -14,67 +13,26 @@ import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-import com.example.surveysystemtext.controller.ChatController;
 
 import java.net.Proxy;
+import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/Survey")
 public class SurveyController {
+    Proxy proxy = Proxys.http("127.0.0.1", 7890);
     @Autowired
     private SurveyMapper surveyMapper;
-
     @Resource
     private OptionMapper optionMapper;
-
     @Resource
     private QuestionMapper questionMapper;
-
-    @GetMapping("/FindAllSurvey")//查询所有问卷信息
-    public CommonResult<List<Survey>> findAllSurvey() {
-        return new CommonResult<>(surveyMapper.findAllSurvey());
-    }
-
-    @PostMapping("/InsertSurvey")//添加问卷 输入完整问卷信息
-    public CommonResult<Survey> InsertSurvey(@RequestBody Survey survey) {
-        var ssid = survey.getSurveyId();
-        if (surveyMapper.selectById(ssid) != null) {
-            return new CommonResult<>(400, "添加失败，问卷已存在");
-        }
-        surveyMapper.insert(survey);
-        var sid = survey.getSurveyId();
-        /*
-         * 插入所有问题
-         */
-        for (var q : survey.getQuestionList()) {
-            /*
-             * 插入所有选项
-             */
-            q.setSurveyId(sid);
-            questionMapper.insert(q);
-            var qid = q.getQuestionId();
-
-            for (var o : q.getOptionList()) {
-                o.setQuestionId(qid);
-                optionMapper.insert(o);
-            }
-        }
-        return new CommonResult<>(survey);
-    }
-
-    @PostMapping("/UpdataSurvey")//修改问卷信息 输入完整问卷信息
-    public CommonResult<Survey> UpdateSurvey(@RequestBody Survey survey) {
-        var sid = survey.getSurveyId();
-        if (surveyMapper.selectById(sid) == null) {
-            return new CommonResult<>(400, "更新失败，问卷不存在");
-        }
-        DeleteSurveyById(survey);
-        InsertSurvey(survey);
-        Survey survey1 = surveyMapper.FindAllSurveyInfo(survey);
-        return new CommonResult<>(survey1);
-    }
+    @Value("${chatgpt.apiKey}")
+    private String apiKey;
+    @Value("${chatgpt.apiHost}")
+    private String apiHost;
 
 //    @PostMapping("/UpdataSurvey")//修改问卷信息 输入完整问卷信息
 //    public CommonResult<Survey> UpdateSurvey(@RequestBody Survey survey){
@@ -189,6 +147,130 @@ public class SurveyController {
 //        return new CommonResult<>(0, "修改成功");
 //    }
 
+    @GetMapping("/FindAllSurvey")//查询所有问卷信息
+    public CommonResult<List<Survey>> findAllSurvey() {
+        return new CommonResult<>(surveyMapper.findAllSurvey());
+    }
+
+//    @PostMapping("/InsertModel")//添加模板
+//    public CommonResult<Survey> InsertModel(@RequestBody Survey survey) {
+//        var sid = survey.getSurveyId();
+//        if (surveyMapper.selectById(sid) == null) {
+//            return new CommonResult<>(400, "查询失败，问卷不存在");
+//        }
+//        //survey1id为新问卷的id，实现自增
+//        Survey survey1 = new Survey();
+//        surveyMapper.insert(survey1);
+//        var sid1 = survey1.getSurveyId();
+//        survey1.setSurveyTitle(survey.getSurveyTitle());
+//        /*
+//         *将survey的所有问题复制给survey1
+//         */
+//        List<Question> questionList = survey.getQuestionList();
+//        for (var q : questionList) {
+//            q.setSurveyId(sid1);
+//            questionMapper.insert(q);
+//            var qid = q.getQuestionId();
+//            /*
+//             * 将survey的所有选项复制给survey1
+//             */
+//            List<Option> optionList = q.getOptionList();
+//            for (var o : optionList) {
+//                o.setQuestionId(qid);
+//                optionMapper.insert(o);
+//            }
+//        }
+//
+//        //将status设为4，表示为模板
+//        survey1.setState(4);
+//        //createdUserId为创建者的id
+//        survey1.setCreatedUserId(survey.getCreatedUserId());
+//        return new CommonResult<>(survey1);
+//    }
+
+    @PostMapping("/CreateTemplate") // 通过问卷生成模板
+    public CommonResult<Survey> CreateTemplate(@RequestBody Survey survey) {
+        // 通过surveyId查找问卷
+        Survey oldSurvey = surveyMapper.selectById(survey.getSurveyId());
+        if (oldSurvey == null) {
+            return new CommonResult<>(400, "生成模板失败，问卷不存在");
+        }
+
+        // 设置新模板属性
+        survey.setCreatedUserId(1L);
+        survey.setSurveyId(null); // 设置为null以实现自增
+        survey.setState(4); // 设置为4
+        survey.setAnalysis(null); // 去掉原有分析
+        survey.setRemark(survey.getRemark()); // 设置备注
+        survey.setSurveyTitle(oldSurvey.getSurveyTitle()); // 设置标题
+
+        // 插入新模板
+        surveyMapper.insert(survey);
+        var sid = survey.getSurveyId();
+        /*
+         *  将oldSurvey进行插入到新模板中
+         */
+        List<Question> newQuestionList = new ArrayList<>();
+        List<Question> questionList = oldSurvey.getQuestionList();
+        if (questionList != null) {
+            for (var q : questionList) {
+                q.setSurveyId(sid);
+                questionMapper.insert(q);
+                var qid = q.getQuestionId();
+                List<Option> optionList = q.getOptionList();
+                if (optionList != null) {
+                    for (var o : optionList) {
+                        o.setQuestionId(qid);
+                        optionMapper.insert(o);
+                    }
+                }
+                newQuestionList.add(q);
+            }
+        }
+        survey.setQuestionList(newQuestionList);
+        return new CommonResult<>(survey);
+    }
+
+
+    @PostMapping("/InsertSurvey")//添加问卷 输入完整问卷信息
+    public CommonResult<Survey> InsertSurvey(@RequestBody Survey survey) {
+        var ssid = survey.getSurveyId();
+        if (surveyMapper.selectById(ssid) != null) {
+            return new CommonResult<>(400, "添加失败，问卷已存在");
+        }
+        surveyMapper.insert(survey);
+        var sid = survey.getSurveyId();
+        /*
+         * 插入所有问题
+         */
+        for (var q : survey.getQuestionList()) {
+            /*
+             * 插入所有选项
+             */
+            q.setSurveyId(sid);
+            questionMapper.insert(q);
+            var qid = q.getQuestionId();
+
+            for (var o : q.getOptionList()) {
+                o.setQuestionId(qid);
+                optionMapper.insert(o);
+            }
+        }
+        return new CommonResult<>(survey);
+    }
+
+    @PostMapping("/UpdataSurvey")//修改问卷信息 输入完整问卷信息
+    public CommonResult<Survey> UpdateSurvey(@RequestBody Survey survey) {
+        var sid = survey.getSurveyId();
+        if (surveyMapper.selectById(sid) == null) {
+            return new CommonResult<>(400, "更新失败，问卷不存在");
+        }
+        DeleteSurveyById(survey);
+        InsertSurvey(survey);
+        Survey survey1 = surveyMapper.FindAllSurveyInfo(survey);
+        return new CommonResult<>(survey1);
+    }
+
     @PostMapping("/FindSurveyInfoById")//根据id查询问卷的所有问题和选项
     public CommonResult<Survey> FindSurveyInfoById(@RequestBody Survey survey) {
         var sid = survey.getSurveyId();
@@ -240,16 +322,11 @@ public class SurveyController {
         return new CommonResult<>(survey);
     }
 
-    Proxy proxy = Proxys.http("127.0.0.1", 7890);
-    @Value("${chatgpt.apiKey}")
-    private String apiKey;
-    @Value("${chatgpt.apiHost}")
-    private String apiHost;
     @PostMapping("/GetSurveyAnalysis") //根据问卷id获取问卷分析
     public CommonResult<DataMessage> GetSurveyAnalysis(@RequestBody Survey survey) {
         String re = "";
         Survey survey1 = surveyMapper.FindAllSurveyInfo(survey);
-        if(survey1 == null){
+        if (survey1 == null) {
             return new CommonResult<>(401, "获取失败");
         }
         List<Question> questionList = survey1.getQuestionList();
@@ -282,7 +359,7 @@ public class SurveyController {
                 .build()
                 .init();
 
-        Message message = Message.of("帮我分析下列给出的数据" + dataMessage.getPrompt());
+        Message message = Message.of("给你一段json格式的数据，这是份问卷的数据，帮我分析下列给出的数据并且以自然语言去描述，具体分析下有多少人选了哪些选项，选项的占比是多少，并以此得出结论，以下是给出的数据：" + dataMessage.getPrompt());
         ChatCompletion chatCompletion = ChatCompletion.builder()
                 .model(ChatCompletion.Model.GPT_3_5_TURBO.getName())
                 .messages((List.of(message)))
